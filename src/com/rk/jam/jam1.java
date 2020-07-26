4fb65edd1dcc0bf1b3fe0e3516bd401cd97470e6
Rizky Khapidsyah <45317201+RizkyKhapidsyah@users.noreply.github.com>
2020-07-26 12:27:02 +0700

    Initial commit

---
+++ src/com/rk/jam/jam1.java
@@ -0,0 +1,29 @@
+package com.rk.jam;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import java.util.Date;
+
+public class jam1 extends Activity implements View.OnClickListener {
+	Button btn;
+
+	@Override
+	public void onCreate(Bundle icicle) {
+		super.onCreate(icicle);
+
+		btn = new Button(this);
+		btn.setOnClickListener(this);
+		updateTime();
+		setContentView(btn);
+	}
+
+	public void onClick(View view) {
+		updateTime();
+	}
+
+	private void updateTime() {
+		btn.setText(new Date().toString());
+	}
+}
